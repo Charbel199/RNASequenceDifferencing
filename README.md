2003ad3341d7ac2e9725e46b597fb9026a4e99e9
charbel <charbel-boumaroun@outlook.com>
2021-05-04 20:02:15 +0300

    Added bat run file and read me

---
+++ README.md
@@ -0,0 +1,16 @@
+# RNA Sequence Differencing
+
+RNA Sequence differencing utilizes Tree edit distance algorithms and Information retrieval algorithms to compute similarities
+and integrate them in a search system.
+
+
+
+## Requirements
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Open *run.bat* and select the desired mode.
---
+++ run.bat
@@ -0,0 +1,32 @@
+@echo off
+title Docker Image updater
+echo Starting ...
+echo IR or TED
+set /P program=Enter mode:
+if "%program%"=="IR"  goto IR
+if "%program%"=="TED"  goto TED
+goto wronginput
+:IR
+cd IR
+goto pythonlogic
+:TED
+cd TED
+goto pythonlogic
+
+
+
+:pythonlogic
+python gui.py
+goto end
+
+:wronginput
+echo Wrong input
+
+
+
+
+
+:end
+
+
+
