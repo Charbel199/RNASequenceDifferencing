97e64f9364c22fc230db8e7cd2fddfd18c03e9f9
charbel <charbel-boumaroun@outlook.com>
2021-05-09 16:38:26 +0300

    added proper IR

--- IR/search/TFIDF.py
+++ IR/search/TFIDF.py
@@ -1,24 +1,72 @@
 
 
 import math
+global IDF_array
+IDF_array = []
+def set_IDF(allElements):
+    global IDF_array
+
+    fullList = []
+    for sequence in allElements:
+        sequence = list(sequence.keys())
+        for el in sequence:
+            if el not in fullList:
+                fullList.append(el)
+    dict = {}
+    for el in fullList:
+        for sequence in allElements:
+            if (el in sequence):
+                try:
+                    dict[el] += 1
+                except:
+                    dict[el] = 1
+
+    IDF_array = dict
+    print('DICT: ')
+    print(dict)
 
 def updateWeights(elements,allElements,TF_method,IDF_method,TF = 1,IDF = 0):
-    if(TF and IDF):
-        TF_measure = TF_method(elements)
-        IDF_measure = IDF_method(elements,allElements)
-        for key in elements:
-            elements[key] = TF_measure[key]*IDF_measure[key]
-        return elements
-    elif(IDF):
-        IDF_measure = IDF_method(elements, allElements)
-        for key in elements:
-            elements[key] = IDF_measure[key]
-        return elements
-    else:
-        TF_measure = TF_method(elements)
-        for key in elements:
-            elements[key] = TF_measure[key]
-        return elements
+    global IDF_array
+    if(len(IDF_array) == 0):
+        set_IDF(allElements)
+
+
+    try:
+        if(TF and IDF):
+            TF_measure = TF_method(elements)
+            IDF_measure = IDF_method(elements,allElements)
+            for key in elements:
+                elements[key] = TF_measure[key]*IDF_measure[key]
+            return elements
+        elif(IDF):
+            IDF_measure = IDF_method(elements, allElements)
+            for key in elements:
+                elements[key] = IDF_measure[key]
+            return elements
+        else:
+            TF_measure = TF_method(elements)
+            for key in elements:
+                elements[key] = TF_measure[key]
+            return elements
+    except:
+        set_IDF(allElements)
+        if (TF and IDF):
+            TF_measure = TF_method(elements)
+            IDF_measure = IDF_method(elements, allElements)
+            for key in elements:
+                elements[key] = TF_measure[key] * IDF_measure[key]
+            return elements
+        elif (IDF):
+            IDF_measure = IDF_method(elements, allElements)
+            for key in elements:
+                elements[key] = IDF_measure[key]
+            return elements
+        else:
+            TF_measure = TF_method(elements)
+            for key in elements:
+                elements[key] = TF_measure[key]
+            return elements
+
 
 
 
@@ -40,42 +88,26 @@ def TF_log(elements):
 
 
 def IDF_log(elements, allElements):
+    global  IDF_array
 
     N = len(allElements)
     for key in elements:
-        counter = 0
-        for element in allElements:
-            try:
-                _ = element[key]
-                counter+=1
-            except:
-                continue
+        counter = IDF_array[key]
         elements[key] = math.log(N/(counter))
     return elements
 
 
 def IDF_log_plus_one(elements, allElements):
+    global IDF_array
     N = len(allElements)
     for key in elements:
-        counter = 0
-        for element in allElements:
-            try:
-                _ = element[key]
-                counter += 1
-            except:
-                continue
+        counter = IDF_array[key]
         elements[key] = math.log(N / (counter))+1
     return elements
 
 def IDF_absolute_log(elements, allElements):
-
+    global IDF_array
     for key in elements:
-        counter = 0
-        for element in allElements:
-            try:
-                _ = element[key]
-                counter += 1
-            except:
-                continue
+        counter = IDF_array[key]
         elements[key] =abs(-math.log(counter))
     return elements
